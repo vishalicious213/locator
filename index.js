7571afba7a50011d792c9eb167cbabef09cf2353
vishalicious213 <vishalicious213@gmail.com>
2023-06-16 01:15:22 -0700

    drafted getWeather()

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Locator App")
+// ⬇️ HELPERS ⬇️
+
+async function getWeather() {
+    try {
+        const position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject)
+        })
+
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+
+        const response = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial`)
+
+        if (!response.ok) {
+            throw new Error("Weather data not available")
+        }
+
+        const data = await response.json()
+        renderWeather(data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+// ⬇️ RENDER APP ⬇️
+
